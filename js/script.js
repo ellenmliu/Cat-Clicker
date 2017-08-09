7c5809f7588b774b4bfb69de8bfa8a14e0733b11
Ellen Liu <ellen.m.liu@gmail.com>
2017-08-09 11:11:26 -0700

    Add grow and shrink animation when image clicked
    Another indication when the user clicked on the cat. Used mouse down
    and up instead of animate to avoid a stack of events lined up.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ $('#cat').click(function(e) {
   clicks += 1;
   $('#numofclicks').text(clicks);
 });
+
+$('#cat').mousedown(function(e) {
+  $('#cat').css({"width" : "+=5", "height":"+=5"})
+});
+
+$('#cat').mouseup(function(e) {
+  $('#cat').css({"width" : "-=5", "height":"-=5"})
+});
