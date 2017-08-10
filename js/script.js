3c3cf46559bd005101850c37cbf40281c4339514
Ellen Liu <ellen.m.liu@gmail.com>
2017-08-09 23:14:43 -0700

    Create cats list and modify script for list
    Stored the values within a list so that the code can use a loop to
    execute same code over for both cats. This way the code is concise
    and efficient. It would help if we were to add in more cats into the
    list and we wouldn't have to add in any more code for it.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 var clicks = 0;
 
-$('#cat').click(function(e) {
-  clicks += 1;
-  $('#numofclicks').text(clicks);
-});
+var cats = [
+  {name: 'Star',
+  img: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+  clicks: 0},
+  {name: 'Tom',
+  img:'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
+  clicks: 0}
+]
 
-$('#cat').mousedown(function(e) {
-  $('#cat').css({"width" : "+=5", "height":"+=5"})
-});
+$(document).ready(function(){
+  cats.forEach(function(cat){
+    $('.container').append('<img id="'+cat.name+'" class="cat" src="'+cat.img+'"/>');
+    $('#'+cat.name).click(function(e) {
+      cat.click += 1;
+    });
 
-$('#cat').mouseup(function(e) {
-  $('#cat').css({"width" : "-=5", "height":"-=5"})
-});
+    $('#'+cat.name).mousedown(function(e) {
+      $('#'+cat.name).css({"width" : "+=5", "height":"+=5"})
+    });
+
+    $('#'+cat.name).mouseup(function(e) {
+      $('#'+cat.name).css({"width" : "-=5", "height":"-=5"})
+    });
+  })
+
+})
