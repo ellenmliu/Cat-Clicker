bea6e8e29962331e4122921b617a1a2913c14b05
Ellen Liu <ellen.m.liu@gmail.com>
2017-08-13 16:41:14 -0700

    Change to use KnockoutJS
    This eliminates the functionality that would grab the data from the
    model and put it in the view because of the synchronous updates that
    Knockout offers. I applied the bindings in the index and added in the
    viewmodel in the app.js file which is more efficient than the last MVO
    structure.
    
    This is to learn how to organize code with model, view, and viewmodel.

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var ViewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Star');
+  this.imgSrc = ko.observable('https://i.ytimg.com/vi/W-PBFMECvTE/maxresdefault.jpg');
+  this.level = ko.observable('Newborn');
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+
+    if (this.clickCount() > 200) {
+      this.level('Adult');
+    }
+    else if (this.clickCount() > 100) {
+      this.level('Teen');
+    }
+    else if (this.clickCount() > 50) {
+      this.level('Kid');
+    }
+    else if (this.clickCount() > 25) {
+      this.level('Toddler');
+    }
+    else if (this.clickCount() > 10) {
+      this.level('Infant');
+    }
+  };
+}
+
+ko.applyBindings(new ViewModel());
