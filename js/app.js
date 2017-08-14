730ce5ead9c0f8928c3faf8b754da0b6e701cb1e
Ellen Liu <ellen.m.liu@gmail.com>
2017-08-13 17:10:01 -0700

    Separate view from viewmodel

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Star');
   this.imgSrc = ko.observable('https://i.ytimg.com/vi/W-PBFMECvTE/maxresdefault.jpg');
-  this.showForm = ko.observable(false);
+
   this.nicknames = ko.observableArray([
     { name: 'Barley'},
     { name: 'Starry Night'},
     { name: 'Constellation'}
   ]);
-
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
 
   this.level = ko.computed(function() {
     if (this.clickCount() > 200) {
@@ -33,6 +29,15 @@ var ViewModel = function() {
       return 'Newborn';
     }
   }, this);
+}
+
+var ViewModel = function() {
+  this.showForm = ko.observable(false);
+  this.currentCat = ko.observable(new Cat());
+
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
 
   this.toggleForm = function() {
     this.showForm(!this.showForm());
